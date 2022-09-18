eafd561f73531341d6c15df575f45364c41007e6
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-18 20:42:50 +0430

    add svg down and up and create style.css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import styles from "./Card.module.css"
+import styles from "./Card.module.css";
+import down from "../image/down.svg";
+import up from "../image/up.svg";
 class Card extends Component {
+    constructor(){
+        super();
+        this.state={
+            counter : 0
+        }
+    }
+    downHandler = ()=>{
+        this.setState(prevState=>({
+            counter :prevState.counter+1
+        }))
+    }
     render() {
-        const {image ,name , cost}=this.props
+        const {image ,name , cost}=this.props;
+        const {counter} =this.state;
         return (
             <div className={styles.container}>
                 <img src={image} alt="card"/>
                 <h3>{name}</h3>
                 <p>{cost}</p>
+                <div className={styles.counter}>
+                     <img src={down} alt="arrow down" onClick={this.downHandler}/>
+                     <span>{counter}</span>
+                     <img src={up} alt="arrow up"/>
+                </div>
              </div>
         );
     }
